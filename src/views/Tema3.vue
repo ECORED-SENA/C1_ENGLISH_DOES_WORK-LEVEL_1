382ebaabc398389fbce4d21cb8f5185899c80aeb
elrilacho <80546125+elrilacho@users.noreply.github.com>
2022-06-28 20:21:11 -0500

    Ajustes listos

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -453,7 +453,7 @@
     Dialogo.color-primario(:dialogo="dialogo")
       template(#nombre-actividad) Arrastrar y Soltar
       template(#titulo-actividad) Greeting the doctor. / Saludando al doctor.
-      template(#descripcion-actividad) Organizar en el orden correcto una conversación.
+      template(#descripcion-actividad) Escuche los audios y complete los espacios en blanco de cada una de las frases arrastrando el término correcto.
 
 
 
